24dd5235a6c064802a28aff363325ad221775b5d
LVTai2995 <23130283@st.hcmuaf.edu.vn>
2026-04-23 17:49:41 +0700

    feat: add SliderImage entity

---
+++ src/main/java/com/mobilestore/entity/SliderImage.java
@@ -0,0 +1,16 @@
+package com.mobilestore.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SliderImage {
+    private Integer id;
+    private String imageUrl;
+    private Boolean isActive;
+}
